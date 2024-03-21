8eb92c9456f01813429cddd031d375cf1fb6a527
gtheo <giorgostheodoraki@hotmail.com>
2024-03-21 09:40:09 +0200

    A SymTable implementation start

--- src/symTable.c
+++ src/symTable.c
@@ -6,5 +6,23 @@
 /* A Hash Map implementation of the symTable interface 		*/
 /*----------------------------------------------------------*/
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <symTable.h>
 
+SymTable_T SymTable_new(void)
+{
+	return NULL;
+}
+
+void SymTable_free(SymTable_T oSymTable)
+{
+	return;
+}
+
+int SymTable_insert(SymTable_T oSymTable,
+					SymEntry_t eSymTable)
+{
+	return EXIT_FAILURE;
+}
