507775d39cd5ce4a35a30acc3aae87567a137a36
gtheo <giorgostheodoraki@hotmail.com>
2024-03-24 13:15:45 +0200

    made scope stack

--- include/ScopeStack.h
+++ include/ScopeStack.h
@@ -1,18 +1,58 @@
-/*----------------------------------------------------------*/
 /* Authors: csd4881, csd4988, csd5038						*/
 /* 														    */
+/*----------------------------------------------------------*/
 /* ScopeStack.h		            							*/
 /* 														    */
-/* A simple interface for a Stack ADT that holds the latest	*/
-/* function scope											*/
+/* A simple interface for an integer Stack module			*/
 /*----------------------------------------------------------*/
 
 #ifndef __SCOPE_STACK__
 #define __SCOPE_STACK__
 
-int ScopeTop();
-int ScopePop();
-int ScopePush();
-int ScopeFree();
+typedef struct ScopeStack ScopeStack, *ScopeStack_T;
+
+struct  ScopeStack
+{
+	unsigned int scope;
+	ScopeStack_T prev;
+};
+
+/**
+* @brief Initialize Stack
+* 
+* @return An empty Stack
+*/
+ScopeStack_T ScopeStack_init(void);
+
+/**
+* @brief Free the stack
+* @param stack The stack to be freed. 
+*/
+void ScopeFree(ScopeStack_T stack);
+
+/**
+* @brief Returns the Top element of the Stack
+* @param stack The stack used. 
+*
+* @return The Top element of the stack (it is an integer)
+*/
+unsigned int ScopeTop(ScopeStack_T stack);
+
+/**
+* @brief Pops out the Top element of the Stack
+* @param stack The stack used. 
+*
+* @return The Top element of the stack (it is an integer)
+*/
+unsigned int ScopePop(ScopeStack_T stack);
+
+/**
+* @brief Push an element in the stack.
+* @param stack The stack used. 
+* @param scope To be pushed. 
+*
+* @return true if it has succeded and false if not 
+*/
+ScopeStack_T ScopePush(ScopeStack_T stack, unsigned int scope);
 
 #endif /* ScopeStack  ADT*/
--- src/ScopeStack.c
+++ src/ScopeStack.c
@@ -5,3 +5,75 @@
 /* 														    */
 /* A implementation of the Scope ADT						*/
 /*----------------------------------------------------------*/
+
+#include <ScopeStack.h>
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Return an Empty Stack or NULL */
+ScopeStack_T ScopeStack_init(void)
+{
+	ScopeStack_T new = malloc(sizeof(ScopeStack));
+
+	/* If malloc fails abort */
+	assert(new);
+
+	new->prev = NULL;
+	new->scope = 0u;
+
+	return new;
+}
+
+/* Free the stack */
+void ScopeFree(ScopeStack_T stack)
+{
+	ScopeStack_T temp;
+	while(stack)
+	{
+		temp = stack;
+		stack = stack->prev;
+		free(temp);
+	}
+}
+
+/* Push an element in the Stack */
+ScopeStack_T ScopePush(ScopeStack_T stack, unsigned int scope)
+{
+	if(stack == NULL)
+		return NULL;
+
+	ScopeStack_T head = malloc(sizeof(ScopeStack));
+	
+	/* If malloc fails abort */	
+	assert(head);
+
+	head->scope = scope;
+	head->prev = stack;
+
+	return head;
+}
+
+/* Get the top element of the stack */
+unsigned int ScopeTop(ScopeStack_T stack)
+{
+	assert(stack);
+	return stack->scope;
+}
+
+/* Pop the top of the stack */
+unsigned int ScopePop(ScopeStack_T stack)
+{
+	assert(stack);
+
+	unsigned int top = ScopeTop(stack);
+	
+	ScopeStack_T temp = stack;
+
+	stack = stack->prev;
+	
+	free(temp);
+
+	return top;
+}
