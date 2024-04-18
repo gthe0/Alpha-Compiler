251e3d12a8c44b106cae77faa5cd0fe117306c40
gtheo <giorgostheodoraki@hotmail.com>
2024-04-18 11:22:22 +0300

    made the start

---
+++ include/expr.h
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------*/
+/* Authors: csd4881, csd4988, csd5038						*/
+/* 														    */
+/* expr.h			            							*/
+/* 														    */
+/* Type definitions and module for the expressions			*/
+/* encountered during parsing								*/
+/*----------------------------------------------------------*/
+
+#ifndef __EXPRESSIONS__
+#define __EXPRESSIONS__ 
+
+#include <symTableEntry.h>
+
+/* typedefs of the various structs */
+typedef enum expr_t expr_t ;
+typedef struct expr expr;
+
+/* expression types */
+enum expr_t
+{
+    var_e,
+    tableitem_e,
+
+    programfunc_e,
+    libraryfunc_e,
+
+    arithexpr_e,
+    boolexpr_e,
+    assignexpr_e,
+    newtable_e,
+
+    constnum_e,
+    constbool_e,
+    conststring_e,
+
+    nil_e,
+    undef_e
+
+};
+
+/* expression list ??? */
+struct expr
+{
+	expr_t type;
+	expr *index;
+	expr *next;
+    
+    SymEntry_T sym;
+    
+	double numConst;
+    char *strConst;
+    unsigned char boolConst;
+};
+
+
+
+#endif /* expr.h module */
