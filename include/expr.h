26739a07434cbd3d53a0ad27de9b346e676d6013
gtheo <giorgostheodoraki@hotmail.com>
2024-04-19 18:45:12 +0300

    some expression functions done

--- include/expr.h
+++ include/expr.h
@@ -14,7 +14,7 @@
 
 /* typedefs of the various structs */
 typedef enum expr_t expr_t ;
-typedef struct expr expr, *Expr_T;
+typedef struct expr expr ;
 
 /* expression types */
 enum expr_t
@@ -52,6 +52,38 @@ struct expr
     char *strConst;
     unsigned char boolConst;
 };
+
+
+/**
+* @brief Creates a new blank expression.
+* @param type The expression type e.g. variable, function, nil e.t.c.
+*
+* @return The expression created
+*/
+expr* newexpr(expr_t type);
+
+/**
+* @brief Creates a new String expression 
+* @param strConst The string used in the expression 
+*
+* @return The String expression
+*/
+expr* new_string_expr(char* strConst);
+
+/**
+* @brief Creates a new boolean expression 
+* @param boolConst Value of the boolean expression  
+*
+* @return The boolean expression
+*/
+expr* new_bool_expr(bool boolConst);
+
+/**
+* @brief Creates a new nil expression 
+*
+* @return The nil expression
+*/
+expr* new_nil_expr(void);
 
 
 #endif /* expr.h ADT */
--- src/expr.c
+++ src/expr.c
@@ -5,3 +5,43 @@
 /* 														    */
 /* Implementation of the expression ADT						*/
 /*----------------------------------------------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <expr.h>
+
+/* Creates a new expression */
+expr *newexpr(expr_t t)
+{
+    expr *e = (expr *)malloc(sizeof(expr));
+    memset(e, 0, sizeof(expr));
+
+    e->type = t;
+    return e;
+}
+
+
+/*Creates a new string expression*/
+expr* new_string_expr(char* string)
+{
+	expr* e = newexpr(conststring_e);
+	e->strConst = string;
+	return e;
+}
+
+
+/*Creates a new nil expression*/
+expr* new_nil_expr(void)
+{
+	return newexpr(nil_e);
+}
+
+
+/*Creates a new boolean expression*/
+expr* new_nil_expr(bool boolConst)
+{
+	expr* e = newexpr(boolexpr_e);
+	e->boolConst = boolConst;
+	return e;
+}
